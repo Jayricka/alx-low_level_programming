8d9ef010cd59af9854a0ff08704acdd029b6eaaf
jayricka <jayricka17@gmail.com>
2023-05-12 09:38:58 +0300

    "elf"

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,18 +15,113 @@ void print_error(const char *message)
 void print_elf_header(const Elf64_Ehdr *header)
 {
     int i;
-    printf("ELF Header:\n");
-    printf("  Magic:   ");
+    printf("Magic:   ");
     for (i = 0; i < EI_NIDENT; i++)
         printf("%02x ", header->e_ident[i]);
     printf("\n");
-    printf("  Class:                             %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
-    printf("  Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
-    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-    printf("  OS/ABI:                            %s\n", (header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ? "UNIX - System V" : "UNIX - Other");
-    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-    printf("  Type:                              %s\n", (header->e_type == ET_EXEC) ? "EXEC (Executable file)" : ((header->e_type == ET_DYN) ? "DYN (Shared object file)" : "Other"));
-    printf("  Entry point address:               %lx\n", header->e_entry);
+    printf("Class:   ");
+    switch (header->e_ident[EI_CLASS])
+    {
+        case ELFCLASSNONE:
+            printf("None\n");
+            break;
+        case ELFCLASS32:
+            printf("ELF32\n");
+            break;
+        case ELFCLASS64:
+            printf("ELF64\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+    printf("Data:    ");
+    switch (header->e_ident[EI_DATA])
+    {
+        case ELFDATANONE:
+            printf("None\n");
+            break;
+        case ELFDATA2LSB:
+            printf("2's complement, little endian\n");
+            break;
+        case ELFDATA2MSB:
+            printf("2's complement, big endian\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+    printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
+    printf("OS/ABI:  ");
+    switch (header->e_ident[EI_OSABI])
+    {
+        case ELFOSABI_NONE:
+            printf("UNIX - System V\n");
+            break;
+        case ELFOSABI_HPUX:
+            printf("UNIX - HP-UX\n");
+            break;
+        case ELFOSABI_NETBSD:
+            printf("UNIX - NetBSD\n");
+            break;
+        case ELFOSABI_LINUX:
+            printf("UNIX - Linux\n");
+            break;
+        case ELFOSABI_SOLARIS:
+            printf("UNIX - Solaris\n");
+            break;
+        case ELFOSABI_AIX:
+            printf("UNIX - AIX\n");
+            break;
+        case ELFOSABI_IRIX:
+            printf("UNIX - IRIX\n");
+            break;
+        case ELFOSABI_FREEBSD:
+            printf("UNIX - FreeBSD\n");
+            break;
+        case ELFOSABI_TRU64:
+            printf("UNIX - TRU64\n");
+            break;
+        case ELFOSABI_MODESTO:
+            printf("Novell - Modesto\n");
+            break;
+        case ELFOSABI_OPENBSD:
+            printf("UNIX - OpenBSD\n");
+            break;
+        case ELFOSABI_ARM:
+            printf("ARM\n");
+            break;
+        case ELFOSABI_STANDALONE:
+            printf("Standalone (embedded) application\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+    printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
+    printf("Type:     ");
+    switch (header->e_type)
+    {
+        case ET_NONE:
+            printf("None\n");
+            break;
+        case ET_REL:
+            printf("REL (Relocatable file)\n");
+            break;
+        case ET_EXEC:
+            printf("EXEC (Executable file)\n");
+            break;
+        case ET_DYN:
+            printf("DYN (Shared object file)\n");
+            break;
+        case ET_CORE:
+            printf("CORE (Core file)\n");
+            break;
+        default:
+            printf("Unknown\n");
+            break;
+    }
+    printf("Entry point address: %lx\n", header->e_entry);
 }
 
 int main(int argc, char *argv[])
@@ -55,4 +150,3 @@ int main(int argc, char *argv[])
     close(fd);
     return 0;
 }
-
