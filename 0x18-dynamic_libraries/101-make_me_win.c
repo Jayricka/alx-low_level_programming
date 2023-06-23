68c866273d832af7bb3382cd8dca3cdfb1134e37
jayricka <jayricka17@gmail.com>
2023-06-24 02:58:30 +0300

    debug

--- 0x18-dynamic_libraries/101-make_me_win.c
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -4,19 +4,19 @@
 #include <string.h>
 
 /**
- * puts - Overrides the puts function to display a winning message.
- * @str: The string to be displayed (unused).
- *
- * Return: Always returns the result of the original puts function.
- */
+* puts - Overrides the puts function to display a winning message.
+* @str: The string to be displayed (unused).
+*
+* Return: Always returns the result of the original puts function.
+*/
 typedef int (*puts_func)(const char *);
 
 int puts(const char *str __attribute__((unused)))
 {
-    puts_func original_puts;
-    void *handle = dlopen("libc.so.6", RTLD_LAZY);
-    *(void **)(&original_puts) = dlsym(handle, "puts");
-    dlclose(handle);
+puts_func original_puts;
+void *handle = dlopen("libc.so.6", RTLD_LAZY);
+*(void **)(&original_puts) = dlsym(handle, "puts");
+dlclose(handle);
 
-    return original_puts("Congratulations! You won the Jackpot!");
+return (original_puts("Congratulations! You won the Jackpot!"));
 }
