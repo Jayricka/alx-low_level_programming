fe04634c44e1f5df040dc30e0330ca3daea0b488
jayricka <jayricka17@gmail.com>
2023-08-29 07:37:53 +0300

    no more single compilation

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+    
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4  # Add 4 initially for the cell's perimeter
+                # Check neighbors (up, down, left, right)
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2  # Subtract 2 for shared edge
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2  # Subtract 2 for shared edge
+    
+    return perimeter
+
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "m.h"
+
+/**
+ * main - Entry point of a program
+ *
+ * Return: Always 
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
