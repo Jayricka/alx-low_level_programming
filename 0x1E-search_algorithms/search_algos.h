fd5d709e83582adeb16b40ae9fe44b1cfadd11f2
jayricka <jayricka17@gmail.com>
2023-10-11 14:44:11 +0300

    search algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdlib.h>
+
+/* Function prototype for linear_search */
+int linear_search(int *array, size_t size, int value);
+
+/* Add other function prototypes for your search algorithms here */
+
+#endif /* SEARCH_ALGOS_H */
